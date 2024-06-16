515bd0b654158052ed80c1fb4876f49030a0e9c3
Vladimir Privalov <vovaprivalov@gmail.com>
2024-06-16 23:47:03 +0400

    Clean code

--- hole_filling/test_hole_filling.cpp
+++ hole_filling/test_hole_filling.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <pmp/SurfaceMesh.h>
-// #include <pmp/algorithms/SurfaceSimplification.h>
 #include <pmp/algorithms/SurfaceHoleFilling.h>
 
 using namespace std;
 
-int main() {
-    std::string obj_input_file { "map.obj" };
-    std::string obj_output_file { "map_out.obj" };
+int main()
+{
+    std::string obj_input_file{"map.obj"};
+    std::string obj_output_file{"map_out.obj"};
 
     pmp::SurfaceMesh mesh;
     mesh.read(obj_input_file);
@@ -18,11 +18,10 @@ int main() {
     for (auto h : mesh.halfedges())
     {
         if (h.is_valid())
-            cout << "halfedge " << h.idx() << endl;
             surfaceHoleFiller.fill_hole(h);
     }
 
     mesh.write(obj_output_file);
 
-    std::cout << "Done! wrote new mesh to " << obj_output_file << '\n';
+    return 0;
 }
